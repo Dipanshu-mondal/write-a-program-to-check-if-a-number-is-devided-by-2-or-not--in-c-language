905b146db6b72b1d87228cc7948c0c1f5be6b858
Dipanshu Mondal <papiyamondaljr@gmail.com>
2026-09-20 20:06:05 +0530

    Add files via upload
    i write a program in this program we can know that what number can divided by 2 .

---
+++ 2 devided check.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+    int x;
+    printf("enter you number");
+    scanf("%d", &x);
+    printf("%d", x%2 == 0); // that means when X%2 = 0 then equal 0 so show 1 means true
+    return 0;
+}
